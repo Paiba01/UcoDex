d302f1076dddaae66c4f198911a15f428d53c808
pabloccf <palogo2001@hotmail.es>
2022-11-28 18:27:57 +0100

    Redirect home button

--- app/src/main/java/com/example/ucodex/MainActivity.java
+++ app/src/main/java/com/example/ucodex/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ucodex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonClick = (Button) findViewById(R.id.button_inicio);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondScreen.class));
+            }
+        });
     }
 }
